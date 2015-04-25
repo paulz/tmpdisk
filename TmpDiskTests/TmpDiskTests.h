91acf7d49dc203b1e4283b02efe98257f59f2023
Paul Zabelin <paulz@Retina.local>
2015-04-24 23:58:39 -0700

    update project settings to Xcode 6.3 recommended

--- TmpDiskTests/TmpDiskTests.h
+++ TmpDiskTests/TmpDiskTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2011 Ink Scribbles Pty Ltd. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface TmpDiskTests : SenTestCase
+@interface TmpDiskTests : XCTestCase
 
 @end
